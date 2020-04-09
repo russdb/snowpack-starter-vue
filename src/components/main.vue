0ce9ae554e870695975cf5fb2618a48f997f79a0
David <russd@tuta.io>
2020-04-09 15:49:26 -0700

    fixed typo

--- src/components/main.vue
+++ src/components/main.vue
@@ -8,7 +8,7 @@
 module.exports = {
   data: () => ({
     content:
-      "this is using web_modules instead of node_modules and thanks to that, we don't need a bunder!"
+      "this is using web_modules instead of node_modules and thanks to that, we don't need a bundler!"
   })
 };
 </script>
